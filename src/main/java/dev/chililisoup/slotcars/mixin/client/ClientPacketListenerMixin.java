5d7f414661c8b064e8f61d406da5465d06e9f8d7
chililisoup <rebot333gaming@gmail.com>
2025-08-18 20:30:46 -0600

    toss vanilla move packets to authoritative client

--- src/main/java/dev/chililisoup/slotcars/mixin/client/ClientPacketListenerMixin.java
+++ src/main/java/dev/chililisoup/slotcars/mixin/client/ClientPacketListenerMixin.java
@@ -3,11 +3,18 @@ package dev.chililisoup.slotcars.mixin.client;
 import dev.chililisoup.slotcars.client.sounds.SlotCarSoundInstance;
 import dev.chililisoup.slotcars.entity.SlotCar;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundEntityPositionSyncPacket;
+import net.minecraft.network.protocol.game.ClientboundMoveEntityPacket;
+import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Function;
 
 @Mixin(ClientPacketListener.class)
 public abstract class ClientPacketListenerMixin {
@@ -17,5 +24,36 @@ public abstract class ClientPacketListenerMixin {
             ((ClientPacketListener) (Object) this).minecraft.getSoundManager().play(new SlotCarSoundInstance(car));
             ci.cancel();
         }
+    }
+
+    @Unique private boolean slotCars$shouldTossPacket(Entity entity) {
+        return entity instanceof SlotCar car && car.isLocalInstanceAuthoritative();
+    }
+
+    @Unique private boolean slotCars$shouldTossPacket(int id) {
+        return this.slotCars$shouldTossPacket(((ClientPacketListener) (Object) this).getLevel().getEntity(id));
+    }
+
+    @Unique private boolean slotCars$shouldTossPacket(Function<Level, Entity> entitySupplier) {
+        Level level = ((ClientPacketListener) (Object) this).getLevel();
+        return this.slotCars$shouldTossPacket(entitySupplier.apply(level));
+    }
+
+    @Inject(method = "handleMoveEntity", at = @At(value = "HEAD"), cancellable = true)
+    private void tossMoveSync(ClientboundMoveEntityPacket clientboundMoveEntityPacket, CallbackInfo ci) {
+        if (slotCars$shouldTossPacket(clientboundMoveEntityPacket::getEntity))
+            ci.cancel();
+    }
+
+    @Inject(method = "handleEntityPositionSync", at = @At(value = "HEAD"), cancellable = true)
+    private void checkPositionSync(ClientboundEntityPositionSyncPacket clientboundEntityPositionSyncPacket, CallbackInfo ci) {
+        if (slotCars$shouldTossPacket(clientboundEntityPositionSyncPacket.id()))
+            ci.cancel();
+    }
+
+    @Inject(method = "handleSetEntityMotion", at = @At(value = "HEAD"), cancellable = true)
+    private void tossMotionSync(ClientboundSetEntityMotionPacket clientboundSetEntityMotionPacket, CallbackInfo ci) {
+        if (slotCars$shouldTossPacket(clientboundSetEntityMotionPacket.getId()))
+            ci.cancel();
     }
 }
